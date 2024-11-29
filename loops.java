ce41b4a43b14fd046e1b1186c7642efa9b4a878f
iris <109429124+archiristo@users.noreply.github.com>
2024-11-29 12:41:57 +0300

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,20 @@
+
+
+public class loops {
+    public static void main(String[] args) {
+        int i = 0;
+        for(i = 0; i < 10; i++){
+            System.out.println("arch");
+        }
+
+        i = 0;
+        while(i < 10){
+            System.out.println("bs");
+            i++;
+
+        }
+
+    }
+
+    
+}
